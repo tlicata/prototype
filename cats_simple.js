eccd1c948495ec94ddb0a05f7cfd73efafb01723
Tim Licata <tlicata@hotpads.com>
2014-06-23 16:49:19 -0700

    add cats_simple

---
+++ cats_simple.js
@@ -0,0 +1,11 @@
+var Cat = function (name) {
+    this.name = name;
+};
+Cat.prototype = {};
+var cat = new Cat("Mr. Whiskers");
+
+// var HouseCat = function (name) {
+//     this.name = name;
+// };
+// HouseCat.prototype = new Cat();
+// var pet = new HouseCat("Mr. Whiskers");
